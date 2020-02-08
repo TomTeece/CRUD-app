a3e6373688c1175a5bbba02731e67c66ff7b54e8
Tom Teece <teece.t@gmail.com>
2020-02-08 11:45:10 +0000

    added listening port

--- app.js
+++ app.js
@@ -97,3 +97,8 @@ db.connect((err)=> {
     }
 })
 
+// Listen to the App Engine-specified port, or 8080 otherwise
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}...`);
+});
